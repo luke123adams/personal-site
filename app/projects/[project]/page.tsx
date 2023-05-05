12d96f200f7474336b87038b46fafad0eb3d18f7
luke123adams <lxa207@gmail.com>
2023-05-05 16:55:18 +0100

    started building product page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -9,6 +9,17 @@ export default async function Project({ params }: Props) {
     const project = await getProject(slug);
     
     return (
-        <div>{project.name}</div>
+        <div className="max-w-3x1 mx-auto py-20">
+            <header className="flex items-center justify-between">
+                <h1 className="text-transparent text-7x1 font-extrabold">{project.name}</h1>
+                <a 
+                href={project.url} 
+                title="View Project" 
+                target="blank" 
+                rel="nooperner noreferrer"
+                className="bg-gray-100 rounded-lg text-gray-500 font-bold py3 px4 whitespace-nowrap hover:bg-red-300 hover:text-white-100 transition drop-shadow-md"
+                >View Project</a>
+            </header>
+        </div>
     )
 }
